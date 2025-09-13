942dd6eeede6c72ff3848b8d64df270099ed6123
alelex10 <elvizvida@gmail.com>
2025-09-13 20:38:50 -0300

    feat: install tailwind 4

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -9,10 +9,11 @@ import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-	plugins: [react(), reactRouter(), tsconfigPaths()],
+	plugins: [tailwindcss(), react(), reactRouter(), tsconfigPaths()],
 	test: {
 		projects: [
 			{
