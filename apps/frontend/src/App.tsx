feec8ff4ec60f3be8108039b277db93fd92841a8
alelex10 <elvizvida@gmail.com>
2025-09-13 22:47:18 -0300

    feat:react router funciona

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router";
 import { FormCustom } from "./components/form/FormCustom";
+import { Home } from "./pages/home";
 
 function App() {
-	return <FormCustom />;
+	return (
+		<Routes>
+			<Route index element={<Home />} />
+			{/* <Route path="about" element={<About />} />
+
+			<Route element={<AuthLayout />}>
+				<Route path="login" element={<Login />} />
+				<Route path="register" element={<Register />} />
+			</Route> */}
+
+			{/* <Route path="concerts">
+				<Route index element={<ConcertsHome />} />
+				<Route path=":city" element={<City />} />
+				<Route path="trending" element={<Trending />} />
+			</Route> */}
+		</Routes>
+	);
 }
 
 export default App;
